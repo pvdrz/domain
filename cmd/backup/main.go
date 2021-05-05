f94337ed60ecfa322b66705a4cf6a19d545b5b07
Christian Poveda <git@pvdrz.com>
2021-05-05 18:41:07 -0500

    remove unnecessary division

--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"encoding/json"
+	"github.com/pvdrz/domain/lib/doc"
+	"io/ioutil"
+)
+
+type backup struct {
+	Docs []doc.Doc
+}
+
+func loadBackup(path string) ([]doc.Doc, error) {
+	var backup backup
+
+	bytes, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bytes, &backup)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return backup.Docs, nil
+}
+
+func dumpBackup(path string, docs []doc.Doc) error {
+	bytes, err := json.Marshal(backup{Docs: docs})
+
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path, bytes, 0664)
+}
